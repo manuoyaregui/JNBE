2b0a87468d71dc94b993d8d2791fb9e93f7d4f40
Manuel Oyaregui <manu.oyaregui@gmail.com>
2021-11-17 20:45:57 -0300

    agrego GameManager de tipo Singleton

---
+++ Assets/Scrips/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int lives;
+    public static GameManager singletonGameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        singletonGameManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddLives()
+    {
+        lives++;
+    }
+    public void RestLives()
+    {
+        lives--;
+    }
+}
