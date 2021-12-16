e87f61f3d503f2f3b2e0392ed2a118df385299df
nenebot <matute866@gmail.com>
2021-12-16 04:07:13 -0300

    UI key guide
    Agregue una guia de los botones en el menu del tutorial

---
+++ Assets/UI/SkipTextGuide.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipTextGuide : MonoBehaviour
+{
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            canvas.enabled = false;
+        }
+    }
+}
