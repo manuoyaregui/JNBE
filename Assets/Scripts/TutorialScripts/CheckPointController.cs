611c72a08dd00376de1d83bb9d28677adcd4d296
Manuel Oyaregui <manu.oyaregui@gmail.com>
2021-12-23 16:08:44 -0300

    arreglo otro par de cosas

--- Assets/Scripts/TutorialScripts/CheckPointController.cs
+++ Assets/Scripts/TutorialScripts/CheckPointController.cs
@@ -1,15 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckPointController : MonoBehaviour
 {
-    private GameObject player;
     [SerializeField] private List<Transform> checkpoints;
     private int currentCheckpoint = 0;
-    [SerializeField] private string KillZoneTagName;
     private PlayerController scriptController;
-    bool isDead = false;
 
 
     // Start is called before the first frame update
@@ -20,9 +16,9 @@ public class CheckPointController : MonoBehaviour
 
     public void MovePlayer() //entraCuandoElPlayerTocaElKillZone
     {
-        transform.position = checkpoints[currentCheckpoint].position + new Vector3(0,2,0); // Lo subo un poco asi no se traba en el gameobject
-        scriptController.isAlive = true;
         scriptController.SetLives(1);
+        scriptController.isAlive = true;
+        transform.position = checkpoints[currentCheckpoint].position + new Vector3(0,2,0); // Lo subo un poco asi no se traba en el gameobject
     }
 
     public void CheckPointReached(int checkpointNumber)
