36e2cfcd569e7d46be18d662c1c9b76815d777f3
Manuel Oyaregui <manu.oyaregui@gmail.com>
2022-01-21 14:11:56 -0300

    probando lerp colors

---
+++ Assets/UI/Scripts/ChangeColorManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorManager : MonoBehaviour
+{
+    [Range(0.01f, 1f)] [SerializeField] float speedTransition = .1f;
+    [ColorUsage(true,true)][SerializeField] Color[] colors;
+    [SerializeField] Material[] materials;
+    [SerializeField] Light backgroundLight;
+
+    int buffer, randomIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ChangeColors", 0, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundLight.color = Color.Lerp(backgroundLight.color, colors[randomIndex], speedTransition);
+    }
+
+    private void ChangeColors()
+    {
+        GenerateRandomIndex();
+        foreach(Material material in materials)
+        {
+            material.SetColor("_EmissionColor", colors[randomIndex]);
+            Color.Lerp(material.GetColor("_EmissionColor"),colors[randomIndex],speedTransition);
+        }
+    }
+
+    private void GenerateRandomIndex()
+    {
+        do
+        {
+            randomIndex = Random.Range(0, colors.Length);
+        } while (buffer == randomIndex);
+
+        buffer = randomIndex;
+    }
+}
