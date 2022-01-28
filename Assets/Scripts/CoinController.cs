ff306157b320b904bbd2011012274a96c1e73d4d
Manuel Oyaregui <manu.oyaregui@gmail.com>
2022-01-28 11:34:17 -0300

    pruebo monedas in gameblocks

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -6,7 +6,10 @@ public class CoinController : MonoBehaviour
 {
     [SerializeField] int value = 1;
 
-
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * 50 * Time.deltaTime, Space.World);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
